6ec33cd775b316ee243e5b105f80e19ebfeac144
Rodrigo Lopes <rodrigoll2001@outlook.com>
2025-10-26 22:18:33 +0000

    fix: allow frontend domain in CORS config

--- src/main/java/com/nicole_portfolio/controller/ProjectsController.java
+++ src/main/java/com/nicole_portfolio/controller/ProjectsController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/projects")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200", "https://nicole-portfolio-backend.onrender.com"})
 public class ProjectsController {
 
     @Autowired
